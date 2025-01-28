fd811490ddf570532938704385ec306dcd4dadd9
Kinhao <ericke9maas@gmail.com>
2025-01-28 18:39:53 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/kinhao/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinhao.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
