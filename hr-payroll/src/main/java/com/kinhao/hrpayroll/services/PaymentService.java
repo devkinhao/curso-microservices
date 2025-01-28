dd5433a6872c5d755cda213a301b9a29ed5220c4
Kinhao <ericke9maas@gmail.com>
2025-01-28 19:23:30 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/kinhao/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/kinhao/hrpayroll/services/PaymentService.java
@@ -1,15 +1,33 @@
 package com.kinhao.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.kinhao.hrpayroll.entities.Payment;
+import com.kinhao.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
-	
-	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 250.0, days);
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public Payment getPayment(long workerId, int days) {
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", "" + workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
-	
 
 }
